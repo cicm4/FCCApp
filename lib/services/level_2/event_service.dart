f10d6122ff4f6d1ef50bfe2a57a9559d1554ee9b
cicm4 <camilocorreall44@gmail.com>
2024-01-24 12:28:56 -0500

    made changes to eventService

--- lib/services/level_2/event_service.dart
+++ lib/services/level_2/event_service.dart
@@ -1,19 +1,37 @@
 import 'package:fccapp/services/Level_0/database_service.dart';
 import 'package:fccapp/services/data/event.dart';
 import 'package:fccapp/services/level_2/calendar_service.dart';
+import 'package:flutter/foundation.dart';
 
 class EventService {
   DBService dbs;
-  List<Event> events;
+  Map<String, Event> events;
 
   EventService({required this.dbs, required this.events});
 
-  getEventsInUserCalendar(CalendarService cs) {
+  Future<void> getEventsInUserCalendar(CalendarService cs) async {
     //loops over every event in the calendarService and looks for them in DBService in the events section, then adds them to the events list as new Events
-    for (String event in cs.calendar.scheduledDays) {
-      dbs.getFromDB(path: 'events', data: event).then((value) {
-        events.add(Event.fromMap(value!));
-      });
+    try {
+      for (String event in cs.calendar.scheduledDays) {
+        dbs.getFromDB(path: 'events', data: event).then((value) {
+          events.addAll(<String, Event>{event: Event.fromMap(value!)});
+        });
+      }
+    } catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
     }
+  }
+
+  Event? getEventByName(String name) {
+    try {
+      return events[name];
+    } catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+    }
+    return null;
   }
 }
