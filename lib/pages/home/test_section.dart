e138c099e65e391d1efc27916e480c9d6a71e1ca
cicm <camilocorreall44@gmail.com>
2024-05-19 20:44:53 -0500

    small change to test button

--- lib/pages/home/test_section.dart
+++ lib/pages/home/test_section.dart
@@ -7,7 +7,6 @@ class TestSection extends StatelessWidget {
   Widget build(BuildContext context) {
     return ElevatedButton(
       onPressed: () async {
-        await Navigator.pushNamed(context, '/test');
       },
       style: ElevatedButton.styleFrom(
         backgroundColor: const Color(0xFF0b512d),
